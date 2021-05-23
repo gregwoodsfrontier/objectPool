dd45f5addcc64fda44b509eca3c2495353470705
gregwoodsfrontier <iwannaplaygamesjp@gmail.com>
2021-05-23 21:57:21 +0800

    Object Pool tutotial

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser'
 
 import HelloWorldScene from './scenes/HelloWorldScene'
+import CratesScene from './scenes/crateScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
@@ -12,7 +13,7 @@ const config: Phaser.Types.Core.GameConfig = {
 			gravity: { y: 200 }
 		}
 	},
-	scene: [HelloWorldScene]
+	scene: [CratesScene]
 }
 
 export default new Phaser.Game(config)
